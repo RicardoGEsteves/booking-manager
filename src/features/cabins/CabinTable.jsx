281d12d0fc14fa11730109f86bfc50900095e52b
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-11-19 00:22:16 +0000

    add filter and sort feature to cabinsTable

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -3,27 +3,33 @@ import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
-
-// const TableHeader = styled.header`
-//   display: grid;
-//   grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-//   column-gap: 2.4rem;
-//   align-items: center;
-
-//   background-color: var(--color-grey-50);
-//   border-bottom: 1px solid var(--color-grey-100);
-//   text-transform: uppercase;
-//   letter-spacing: 0.4px;
-//   font-weight: 600;
-//   color: var(--color-grey-600);
-//   padding: 1.6rem 2.4rem;
-// `;
+import { useSearchParams } from "react-router-dom";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
-  const render = (cabin) => <CabinRow key={cabin.id} cabin={cabin} />;
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  //Filter
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filteredCabins;
+  if (filterValue === "all") filteredCabins = cabins;
+  if (filterValue === "no-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  if (filterValue === "with-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+
+  //Sort
+  const sortBy = searchParams.get("sortBy") || "startDate-asc";
+  const [field, direction] = sortBy.split("-");
+  const modifier = direction === "asc" ? 1 : -1;
+  const sortedCabins = filteredCabins.sort(
+    (a, b) => (a[field] - b[field]) * modifier
+  );
+
+  const render = (cabin) => <CabinRow key={cabin.id} cabin={cabin} />;
 
   return (
     <Menus>
@@ -37,7 +43,12 @@ function CabinTable() {
           <div></div>
         </Table.Header>
 
-        <Table.Body data={cabins} render={render} />
+        <Table.Body
+          // data={cabins}
+          // data={filteredCabins}
+          data={sortedCabins}
+          render={render}
+        />
       </Table>
     </Menus>
   );
--- src/ui/TableOperations.jsx
+++ src/ui/TableOperations.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const TableOperations = styled.div`
   display: flex;
