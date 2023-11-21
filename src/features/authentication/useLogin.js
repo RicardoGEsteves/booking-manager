41588555f7755bf5995baba3fe268a68a8b71771
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-11-21 00:09:42 +0000

    feat: :sparkles: logout feature
    implement logout feature, create custom hook to mutate the state of the app, redirect user to login page, creal localstorage token and queries, create the api call,  introduce logout component to hearder component and fix a bug on login mutation
    
    fix login bug

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -10,8 +10,8 @@ export function useLogin() {
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
-      queryClient.setQueriesData(["user"], user);
-      navigate("/dashboard");
+      queryClient.setQueryData(["user"], user.user);
+      navigate("/dashboard", { replace: true });
     },
     onError: (err) => {
       console.log("ERROR", err);
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import Logout from "../features/authentication/Logout";
 
 const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
@@ -7,7 +8,11 @@ const StyledHeader = styled.header`
 `;
 
 function Header() {
-  return <StyledHeader>HEADER</StyledHeader>;
+  return (
+    <StyledHeader>
+      <Logout />
+    </StyledHeader>
+  );
 }
 
 export default Header;
