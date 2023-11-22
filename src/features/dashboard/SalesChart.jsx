c2e3de6d88e3662187ace9ea50c6689d9e62f579
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-11-22 16:28:05 +0000

    test: :pencil2: change title
    change the incorrect title to display a more complete one
    
    chart area title with more description

--- src/features/dashboard/SalesChart.jsx
+++ src/features/dashboard/SalesChart.jsx
@@ -90,7 +90,10 @@ function SalesChart({ bookings, numDays }) {
       };
   return (
     <StyledSalesChart>
-      <Heading as="h2">Sales</Heading>
+      <Heading as="h2">
+        Sales from {format(allDates.at(0), "MMM dd yyyy")} &mdash; {""}
+        {format(allDates.at(-1), "MMM dd yyyy")}
+      </Heading>
 
       <ResponsiveContainer height={300} width="100%">
         <AreaChart data={data}>
