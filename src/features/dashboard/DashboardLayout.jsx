ef05948ea277c5c4344f12ff2340db1961e9a809
Ricardo Esteves <ricardoglsesteves@gmail.com>
2023-11-22 20:22:21 +0000

    feat: :sparkles: Pie chart
    introduce Pie chart to the dashboard layout, create a pie chart for the duration of stays
    
    users can now see a pie chart on the dashboard

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -5,6 +5,7 @@ import Spinner from "../../ui/Spinner";
 import Stats from "./stats";
 import { useCabins } from "../cabins/useCabins";
 import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -35,7 +36,7 @@ function DashboardLayout() {
         cabinCount={cabins?.length}
       />
       <div>Today's activity</div>
-      <div>Chart stay duration</div>
+      <DurationChart confirmedStays={confirmedStays} />
       <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
