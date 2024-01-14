35afed8c0a3bac5f0050677d23272e011cdffbd1
Ricardo Esteves <ricardoglsesteves@gmail.com>
2024-01-14 17:31:12 +0000

    chore: :coffin: Header: Refactor code and remove unused components
    Body: This commit includes several changes to improve the codebase:
    - `src/App.jsx`: The main application component has been updated to improve performance and readability.
    - `src/contexts/DarkModeContext.jsx`: The Dark Mode context has been refactored for better state management.
    - `src/data/Uploader.jsx`: The Uploader component has been updated to handle file uploads more efficiently.
    - `src/data/data-guests.js`: The guest data file has been modified to include additional guest details.
    - `src/features/cabins/CreateCabinForm.jsx`:
    
    Unused code in the Create Cabin Form component has been removed to clean up the codebase.
    
    These changes aim to improve the overall quality of the code and enhance the application's performance.
    Footer: Related to: #60

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,6 @@ import { DarkModeProvider } from "./contexts/DarkModeContext";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      // staleTime: 60 * 1000,
       staleTime: 0,
     },
   },
@@ -44,18 +43,56 @@ function App() {
                 </ProtectedRoute>
               }
             >
-              <Route index element={<Navigate replace to="dashboard" />} />
-              <Route path="dashboard" element={<Dashboard />} />
-              <Route path="bookings" element={<Bookings />} />
-              <Route path="bookings/:bookingId" element={<Booking />} />
-              <Route path="checkin/:bookingId" element={<Checkin />} />
-              <Route path="cabins" element={<Cabins />} />
-              <Route path="users" element={<Users />} />
-              <Route path="settings" element={<Settings />} />
-              <Route path="account" element={<Account />} />
+              <Route
+                index
+                element={
+                  <Navigate
+                    replace
+                    to="dashboard"
+                  />
+                }
+              />
+              <Route
+                path="dashboard"
+                element={<Dashboard />}
+              />
+              <Route
+                path="bookings"
+                element={<Bookings />}
+              />
+              <Route
+                path="bookings/:bookingId"
+                element={<Booking />}
+              />
+              <Route
+                path="checkin/:bookingId"
+                element={<Checkin />}
+              />
+              <Route
+                path="cabins"
+                element={<Cabins />}
+              />
+              <Route
+                path="users"
+                element={<Users />}
+              />
+              <Route
+                path="settings"
+                element={<Settings />}
+              />
+              <Route
+                path="account"
+                element={<Account />}
+              />
             </Route>
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<PageNotFound />} />
+            <Route
+              path="login"
+              element={<Login />}
+            />
+            <Route
+              path="*"
+              element={<PageNotFound />}
+            />
           </Routes>
         </BrowserRouter>
 
